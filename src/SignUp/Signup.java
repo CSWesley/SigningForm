55ad29813a55c546449a7b15942f539592453f74
Bo0ked61 <booked1234512@gmail.com>
2021-08-27 22:45:34 -0700

    Added first video tutorial!

---
+++ src/SignUp/Signup.java
@@ -0,0 +1,32 @@
+package SignUp;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Signup {
+
+    public void setSignupInfo(Container c) {
+
+        JTextField username = new JTextField();
+        username.setText("Username");
+        username.setSize(110, 30);
+        username.setLocation(140, 140);
+        c.add(username);
+
+        JTextField password = new JTextField();
+        password.setText("Password");
+        password.setSize(110, 30);
+        password.setLocation(255, 140);
+        c.add(password);
+
+        JComboBox<Object> dob = new JComboBox<>();
+        for (int i = 1; i < 16; i++) {
+            Object object = Integer.toString(i);
+            dob.addItem(object);
+        }
+        dob.setSize(120, 35);
+        dob.setLocation(200, 185);
+        c.add(dob);
+
+    }
+}
