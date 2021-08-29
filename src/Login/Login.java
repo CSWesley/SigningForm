44d31d0b6024fbbbcb763e24efe74a3eb6d48b87
Bo0ked61 <booked1234512@gmail.com>
2021-08-29 11:15:23 -0700

    Episode 2

--- src/Login/Login.java
+++ src/Login/Login.java
@@ -1,15 +1,14 @@
-package SignUp;
+package Login;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class Signup {
+public class Login {
 
     public static JTextField username;
     public static JTextField password;
-    public static JComboBox<Object> dob;
 
-    public void setSignupInfo(Container c, JFrame frame) {
+    public void setLoginInfo(Container c, JFrame frame) {
 
         username = new JTextField();
         username.setText("Username");
@@ -22,15 +21,6 @@ public class Signup {
         password.setSize(110, 30);
         password.setLocation(255, 140);
         c.add(password);
-
-        dob = new JComboBox<>();
-        for (int i = 5; i < 101; i++) {
-            Object object = Integer.toString(i);
-            dob.addItem(object);
-        }
-        dob.setSize(120, 35);
-        dob.setLocation(200, 185);
-        c.add(dob);
 
         frame.setVisible(true);
 
--- src/MainPage/MainPage.java
+++ src/MainPage/MainPage.java
@@ -1,14 +1,20 @@
 package MainPage;
 
+import Login.Login;
 import SignUp.Signup;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.SimpleDateFormat;
 
 public class MainPage {
 
     JButton signup;
+    JButton back;
     JFrame mainFrame;
+    JButton login;
+    JButton confirmLogin;
+    JButton confirmSignup;
 
     public MainPage() {
         mainFrame = new JFrame("Main Signing Form");
@@ -17,17 +23,35 @@ public class MainPage {
         Container c = mainFrame.getContentPane();
         c.setLayout(null);
 
-        JButton login = new JButton("Login");
+        confirmLogin = new JButton("Confirm Login");
+        confirmLogin.setSize(140, 30);
+        confirmLogin.setLocation(165, 185);
+        confirmLogin.addActionListener(e -> {
+            if (e.getSource() == confirmLogin) {
+                // Check if exists.
+            }
+        });
+        confirmLogin.setVisible(false);
+        c.add(confirmLogin);
+
+        confirmSignup = new JButton("Confirm Signup");
+        confirmSignup.setSize(140, 30);
+        confirmSignup.setLocation(165, 225);
+        confirmSignup.addActionListener(e -> {
+            if (e.getSource() == confirmSignup) {
+                // Log into SQL.
+            }
+        });
+        confirmSignup.setVisible(false);
+        c.add(confirmSignup);
+
+        login = new JButton("Login");
         login.setSize(100, 40);
         login.setLocation(100, 200);
         login.addActionListener(e -> {
             if (e.getSource() == login) {
-                Timer timer = new Timer(5, e1 -> {
-                    login.setLocation(login.getX() - 5, 200);
-                    signup.setLocation(signup.getX() - 5, 200);
-                });
-                timer.start();
-                this.showSignup(c);
+                showLogin(c);
+                confirmLogin.setVisible(true);
             }
         });
         c.add(login);
@@ -37,14 +61,48 @@ public class MainPage {
         signup.setLocation(225, 200);
         signup.addActionListener(e -> {
             if (e.getSource() == signup) {
-                Timer timer = new Timer(5, e1 -> {
-                    signup.setLocation(signup.getX() - 5, 200);
-                    login.setLocation(login.getX() - 5, 200);
-                });
-                timer.start();
+                this.showSignup(c);
+                confirmSignup.setVisible(true);
             }
         });
         c.add(signup);
+
+        back = new JButton("Back");
+        back.setSize(110, 30);
+        back.setLocation(200, 400);
+        back.setVisible(false);
+        back.addActionListener(e -> {
+            if (e.getSource() == back) {
+                if (Signup.username != null) {
+                    hideAllSignups();
+                    back.setVisible(false);
+
+                    login.setVisible(true);
+                    signup.setVisible(true);
+                    if (Login.password != null) {
+                        hideAllLogins();
+                        back.setVisible(false);
+
+                        login.setVisible(true);
+                        signup.setVisible(true);
+                    }
+                } else if (Login.username != null) {
+                    hideAllLogins();
+                    back.setVisible(false);
+
+                    login.setVisible(true);
+                    signup.setVisible(true);
+                    if (Signup.password != null) {
+                        hideAllSignups();
+                        back.setVisible(false);
+
+                        login.setVisible(true);
+                        signup.setVisible(true);
+                    }
+                }
+            }
+        });
+        c.add(back);
 
 
         mainFrame.setSize(500, 500);
@@ -52,12 +110,34 @@ public class MainPage {
         mainFrame.setVisible(true);
     }
 
-    public void showSignup(Container c) {
-
-        Signup signup = new Signup();
-        signup.setSignupInfo(c, mainFrame);
-
+    private void hideAllLogins() {
+        Login.username.setVisible(false);
+        Login.password.setVisible(false);
+        confirmLogin.setVisible(false);
     }
 
+    private void hideAllSignups() {
+        Signup.password.setVisible(false);
+        Signup.username.setVisible(false);
+        Signup.dob.setVisible(false);
+        confirmSignup.setVisible(false);
+    }
 
+    public void showSignup(Container c) {
+        Signup signupT = new Signup();
+        signupT.setSignupInfo(c, mainFrame);
+        back.setVisible(true);
+
+        login.setVisible(false);
+        signup.setVisible(false);
+    }
+
+    public void showLogin(Container c) {
+        Login loginT = new Login();
+        loginT.setLoginInfo(c, mainFrame);
+        back.setVisible(true);
+
+        login.setVisible(false);
+        signup.setVisible(false);
+    }
 }
